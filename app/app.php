efc6b0a5934b99fba6b7c061b6313c03d2560884
bpesquet <bpesquet@gmail.com>
2014-07-24 18:36:43 +0200

    Manage article details

--- app/app.php
+++ app/app.php
@@ -16,3 +16,8 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app['dao.article'] = $app->share(function ($app) {
     return new MicroCMS\DAO\ArticleDAO($app['db']);
 });
+$app['dao.comment'] = $app->share(function ($app) {
+    $commentDAO = new MicroCMS\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.article']);
+    return $commentDAO;
+});
--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,14 @@
 <?php
 
-// Return all articles
+// Returns all articles
 $app->get('/', function () use ($app) {
     $articles = $app['dao.article']->findAll();
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
+});
+
+// Returns detailed info about an article
+$app->get('/article/{id}', function ($id) use ($app) {
+    $article = $app['dao.article']->find($id);
+    $comments = $app['dao.comment']->findAllByArticle($id);
+    return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
 });
