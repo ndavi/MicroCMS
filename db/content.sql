e16b45191b54fc74a1c5921ef9f1d84e8c71410b
bpesquet <bpesquet@gmail.com>
2014-09-19 12:12:44 +0200

    Create hashed passwords

--- db/content.sql
+++ db/content.sql
@@ -5,10 +5,12 @@ insert into t_article(art_title, art_content) values
 insert into t_article(art_title, art_content) values
 ('Lorem ipsum in french', "J’en dis autant de ceux qui, par mollesse d’esprit, c’est-à-dire par la crainte de la peine et de la douleur, manquent aux devoirs de la vie. Et il est très facile de rendre raison de ce que j’avance. Car, lorsque nous sommes tout à fait libres, et que rien ne nous empêche de faire ce qui peut nous donner le plus de plaisir, nous pouvons nous livrer entièrement à la volupté et chasser toute sorte de douleur ; mais, dans les temps destinés aux devoirs de la société ou à la nécessité des affaires, souvent il faut faire divorce avec la volupté, et ne se point refuser à la peine. La règle que suit en cela un homme sage, c’est de renoncer à de légères voluptés pour en avoir de plus grandes, et de savoir supporter des douleurs légères pour en éviter de plus fâcheuses.");
 
+/* raw password is 'john' */
 insert into t_user(usr_name, usr_salt, usr_password, usr_role) values
-('JohnDoe', '1260889385528018eda0a12', 'YXylVBIE3HLEUNQEH5Z5bSua4vpEG0flg2V1OcpWw4wzel1nomjtkoG2XVKpug3R4hD18tI0Uj1r8z/3rXxtNg==', 'ROLE_USER');
+('JohnDoe', 'YcM=A$nsYzkyeDVjEUa7W9K', 'L2nNR5hIcinaJkKR+j4baYaZjcHS0c3WX2gjYF6Tmgl1Bs+C9Qbr+69X8eQwXDvw0vp73PrcSeT0bGEW5+T2hA==', 'ROLE_USER');
+/* raw password is 'jane' */
 insert into t_user(usr_name, usr_salt, usr_password, usr_role) values
-('JaneDoe', '1260889385528018eda0a12', 'YXylVBIE3HLEUNQEH5Z5bSua4vpEG0flg2V1OcpWw4wzel1nomjtkoG2XVKpug3R4hD18tI0Uj1r8z/3rXxtNg==', 'ROLE_USER');
+('JaneDoe', 'dhMTBkzwDKxnD;4KNs,4ENy', 'EfakNLxyhHy2hVJlxDmVNl1pmgjUZl99gtQ+V3mxSeD8IjeZJ8abnFIpw9QNahwAlEaXBiQUBLXKWRzOmSr8HQ==', 'ROLE_USER');
 
 insert into t_comment(art_id, usr_id, com_content) values
 (1, 1, 'Great! Keep up the good work.');
